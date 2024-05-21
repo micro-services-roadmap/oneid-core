379aee3f2a7e9fd4ffdaad9b8e4b913b3bf53c8f
alice52 <zzhang_xz@163.com>
2024-05-21 10:17:47 +0800

    feat(model): add oneid conf model

--- model/oneid.go
+++ model/oneid.go
@@ -1,8 +1,8 @@
 package model
 
 type OneidConf struct {
-	AuthenticationUrl string `json:"authentication_url" form:"authentication_url" query:"authentication_url"` // kong
-	AccessKeyId       string `json:"access_key_id" form:"access_key_id" query:"access_key_id"`                // ak
-	AccessKeySecret   string `json:"access_key_secret" form:"access_key_secret" query:"access_key_secret"`    // sk
+	AuthenticationUrl string `json:"authentication-url" form:"authentication-url" query:"authentication-url"` // kong
+	AccessKeyId       string `json:"access_key_id" form:"access_key_id" query:"access-key-id"`                // ak
+	AccessKeySecret   string `json:"access-key-secret" form:"access-key-secret" query:"access-key-secret"`    // sk
 	Subject           string `json:"subject" form:"subject" query:"subject"`                                  // consumer name
 }
