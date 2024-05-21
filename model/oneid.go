1735a4ce89a0f74ee5d467bcdbff5ab32b23ae68
alice52 <zzhang_xz@163.com>
2024-05-21 11:54:45 +0800

    fix(httpx): refine url schema

--- model/oneid.go
+++ model/oneid.go
@@ -1,8 +1,8 @@
 package model
 
 type OneidConf struct {
-	AuthenticationUrl string `json:"authentication-url" form:"authentication-url" query:"authentication-url"` // kong
-	AccessKeyId       string `json:"access_key_id" form:"access_key_id" query:"access-key-id"`                // ak
-	AccessKeySecret   string `json:"access-key-secret" form:"access-key-secret" query:"access-key-secret"`    // sk
-	Subject           string `json:"subject" form:"subject" query:"subject"`                                  // consumer name
+	AuthenticationUrl string `mapstructure:"authentication-url"  yaml:"authentication-url" json:"authentication-url" form:"authentication-url" query:"authentication-url"` // kong
+	AccessKeyId       string `mapstructure:"access-key-id" yaml:"access-key-id" json:"access-key-id" form:"access-key-id" query:"access-key-id"`                           // ak
+	AccessKeySecret   string `mapstructure:"access-key-secret" yaml:"access-key-secret" json:"access-key-secret" form:"access-key-secret" query:"access-key-secret"`       // sk
+	Subject           string `mapstructure:"subject" yaml:"subject" json:"subject" form:"subject" query:"subject"`                                                         // consumer name
 }
