40f8d6c333336ca493ae2e1ccdb2db3beb062893
alice52 <zzhang_xz@163.com>
2024-06-18 07:06:55 +0800

    feat(jwt): add jwt user base model

--- model/jwt_user.go
+++ model/jwt_user.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+)
 
 type JwtUser struct {
 	Id          *int64    `json:"id"`
@@ -8,4 +11,23 @@ type JwtUser struct {
 	Name        *string   `json:"name"`
 	Email       *string   `json:"email"`
 	AuthorityId *int      `json:"authorityId"`
+}
+
+func (u *JwtUser) UserMarshal() string {
+	marshal, err := json.Marshal(u)
+	if err != nil {
+		panic("Marshal JwtValueOfUser failed: " + err.Error())
+	}
+
+	return string(marshal)
+}
+
+func UserUnMarshal(value string) *JwtUser {
+	var u JwtUser
+	err := json.Unmarshal([]byte(value), &u)
+	if err != nil {
+		panic("Unmarshal claims.Value failed: " + err.Error())
+	}
+
+	return &u
 }
